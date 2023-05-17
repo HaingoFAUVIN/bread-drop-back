bba68fa37117a09cee7b011ebd8e79a6e34a7480
HaingoFAUVIN <hfauvin@yahoo.fr>
2023-05-17 16:43:41 +0200

    OrderController BackOffice WIP relation order bakery

--- src/Controller/Back/OrderController.php
+++ src/Controller/Back/OrderController.php
@@ -2,23 +2,54 @@
 
 namespace App\Controller\Back;
 
+use App\Entity\User;
+use App\Entity\Bakery;
+use App\Entity\Product;
 use App\Repository\OrderRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\BakeryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
- * @Route("/back/commandes")
+ * @Route("/back/bakery")
  */
 class OrderController extends AbstractController
 {
     /**
-     * @Route("/", name="app_back_order_index", methods={"GET"})
+     * @Route("/{id}", name="app_back_order_index", methods={"GET"})
      */
-    public function index(OrderRepository $orderRepository): Response
+    public function index(int $id, BakeryRepository $bakeryRepository): Response
     {
+        //On récupère la boulangerie du manager en cours
+        $bakery = $bakeryRepository->find($id);
+
+        // https://symfony.com/doc/current/doctrine/associations.html#joining-related-records
+        // https://symfony.com/doc/current/doctrine/associations.html#fetching-related-objects
+
+        // on va chercher les commandes passées la boulangerie du gérant en cours
+        $products = $bakery->getProduct();
+
         return $this->render('back/order/index.html.twig', [
-            'orders' => $orderRepository->findAll(),
+            'orders' => $products
         ]);
     }
 }
+
+
+/**
+ * @Route("/back/commandes")
+ */
+// class OrderController extends AbstractController
+// {
+    /**
+     * @Route("/", name="app_back_order_index", methods={"GET"})
+     */
+//     public function index(OrderRepository $orderRepository): Response
+//     {
+//         return $this->render('back/order/index.html.twig', [
+//             'orders' => $orderRepository->findAll(),
+//         ]);
+//     }
+// }

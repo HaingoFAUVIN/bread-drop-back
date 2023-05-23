9416842e4e1f02fa196dc1665599d32a09824c5a
davyvistel <davy89250@gmail.com>
2023-05-23 15:20:42 +0200

    crud des produit  liste , ajouter et afficher ok

---
+++ src/Controller/Back/ProductController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller\Back;
+
+use App\Entity\Bakery;
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/back/boulangerie")
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/{id<\d+>}/produits", name="app_back_product_index", methods={"GET"})
+     */
+    public function index(Bakery $bakery, ProductRepository $productRepository): Response
+    {
+    // on va chercher les produits d'une boulangerie donnée
+    $productByBakery = $productRepository->findBy(['bakery' => $bakery]);
+
+    return $this->render('back/product/index.html.twig', [
+        'products' => $productByBakery,
+        'bakery' => $bakery,
+    ]);
+    }
+
+    /**
+     * @Route("{id<\d+>}/ajouter", name="app_back_product_new", methods={"GET", "POST"})
+     */
+    public function new(Bakery $bakery = null, Request $request, ProductRepository $productRepository): Response
+    {
+        // 404 ?
+        if ($bakery === null) {
+            throw $this->createNotFoundException('Boulangerie non trouvé');
+        }
+
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // on associe le produit a la boulangerie
+            $product->setBakery($bakery);
+
+            $productRepository->add($product, true);
+
+            return $this->redirectToRoute('app_back_product_index', ['id' => $bakery->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/product/new.html.twig', [
+            'product' => $product,
+            'form' => $form,
+            'bakery' => $bakery,
+        ]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}/produit", name="app_back_product_show", methods={"GET"})
+     */
+    public function show(Product $product): Response
+    {
+        return $this->render('back/product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_back_product_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Product $product, ProductRepository $productRepository): Response
+    {
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productRepository->add($product, true);
+
+            return $this->redirectToRoute('app_back_product_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_back_product_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Product $product, ProductRepository $productRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+            $productRepository->remove($product, true);
+        }
+
+        return $this->redirectToRoute('app_back_product_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/ProductType.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType as TypeIntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom du produit',
+                'attr' => [
+                    // le placeholder est un attribut HTML
+                    'placeholder' => 'ex. Pain au chocolat',
+                ]
+            ])
+
+            ->add('price', NumberType::class, [
+                'label' => 'Prix du produit',
+                'attr' => [
+                    // le placeholder est un attribut HTML
+                    'placeholder' => 'ex. 0,90',
+                ]
+            ])
+
+            ->add('stock', TypeIntegerType::class, [
+                'label' => 'Nombre de produit en stock',
+                'attr' => [
+                    // le placeholder est un attribut HTML
+                    'placeholder' => 'ex. 15',
+                ]
+            ])
+
+            ->add('description', TextType::class, [
+                'label' => 'Description du produit',
+                'attr' => [
+                    // le placeholder est un attribut HTML
+                    'placeholder' => 'ex. Un pain au chocolat vegan',
+                ]
+            ])
+
+            ->add('picture', UrlType::class, [
+                'label' => "Photo du produit",
+                'attr' => [
+                    'placeholder' => 'https://picsum.photos/450/300?image=249'
+                ]
+            ])
+
+            ->add('category', EntityType::class, [
+                // l'entité associé à ce champ
+                'class' => Category::class,
+                // la propriété de l'entité à afficher dans le choix
+                'choice_label' => 'name',
+                // les categorie sont un tableau donc array !
+                'multiple' => false,
+                // on ne veut pas de checkboxes
+                'expanded' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
